ed3f54bf0506bdac0bb17373768e3150650d1225
Prerak Patel <prerak.patel@strategio.tech>
2022-08-25 19:10:13 -0400

    adding project

---
+++ tech.strategio.helloworld/src/tech/strategio/helloworld/HelloWorldClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package tech.strategio.helloworld;
+
+/**
+ * @author prerakpatel
+ *
+ */
+public class HelloWorldClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method
+		System.out.println("Hello Prerak");
+
+	}
+
+}
