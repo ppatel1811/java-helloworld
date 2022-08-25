2f031aecdc2002568871e0cd3600e0c60c892342
Prerak Patel <prerak.patel@strategio.tech>
2022-08-25 19:35:55 -0400

    initial commit

---
+++ tech.strategio.helloworld/src/tech/strategio/helloworld/Test.java
@@ -0,0 +1,11 @@
+package tech.strategio.helloworld;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method
+		System.out.println("Hello World");
+
+	}
+
+}
